6858aa38403d93dab9084577a4e3d8524ec6145d
onezeun <78632052+onezeun@users.noreply.github.com>
2022-01-22 23:26:50 +0900

    인프런 for문실습

---
+++ htmlcssjs/for.js
@@ -0,0 +1,19 @@
+//혜지야 도와줘!
+
+var a = 50;
+var zerozeun = 0;
+var onezeun = 0;
+
+for (var i = 1; i <= a; ++i) {
+  if(i%2 == 0) {
+    zerozeun = zerozeun + i
+    console.log("짝수" , i)
+  } else {
+    onezeun = onezeun + i
+    console.log("홀수" , i)
+  };
+};
+
+console.log("짝수의 합 :", zerozeun,"홀수의 합 :", onezeun);
+
+//모르겠어요.....
